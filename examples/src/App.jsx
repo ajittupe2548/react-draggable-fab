db87f080e0b675839510fc7af5a3ba51df7b41d5
Ajit Tupe <ajittupe2548@gmail.com>
2024-09-02 20:38:41 +0530

    Added: Example

---
+++ examples/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from  './App.scss';
+
+const app = () => {
+    return (
+        <h1 className={styles.heading}>DraggableButton Example!</h1>
+    )
+}
+
+export default app;
