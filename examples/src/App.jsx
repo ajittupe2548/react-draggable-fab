f58bfc37b640fe15ebe463a3875bf2bed429256e
Ajit Tupe <ajittupe2548@gmail.com>
2024-09-05 23:20:11 +0530

    Update App.jsx

--- examples/src/App.jsx
+++ examples/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DraggableButton from '../../src/DraggableButton';
 
 const App = () => {
+    const [isVisible, setIsVisible] = useState(true);
     return (
-        <DraggableButton isVisible text='Expanded Text' />
+        <DraggableButton isVisible={isVisible} onClose={() => setIsVisible(false)} text='Expanded Text' />
     )
 }
 
