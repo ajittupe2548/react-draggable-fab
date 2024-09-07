5e0b12cab047f5f47037d28cb8a50bbde19a9862
Ajit Tupe <ajittupe2548@gmail.com>
2024-09-07 19:41:34 +0530

    Handled: Desktop

--- src/DraggableButton.jsx
+++ src/DraggableButton.jsx
@@ -47,115 +47,115 @@ function DraggableButton({
     children,
 }) {
     const [isDragging, setIsDragging] = useState(false);
-
-    useEffect(() => {
-        draggableBtnRef.current.style.opacity = 0.5;
-
-        return () => {
-            document.body.style.height = 'auto';
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
+    const [isTouch, setIsTouch] = useState(false);
+    const isMouseDownRef = useRef(false);
 
     const draggableBtnRef = useRef();
     const closeBtnRef = useRef();
     const isCloseButtonHoveredRef = useRef(false);
 
-    const handleTouchStart = useCallback(() => {
+    useEffect(() => {
+        draggableBtnRef.current.style.opacity = 0.5;
+        window.addEventListener('mousemove', handleMove);
+
+        return () => {
+            document.body.style.height = 'auto';
+            document.body.style.overflow = 'auto';
+            window.removeEventListener('mousemove', handleMove);
+        };
+    }, []);
+
+    const handleStart = useCallback((event) => {
+        setIsTouch(event.type === 'touchstart');
+        if(event.type !== 'touchstart') {
+            isMouseDownRef.current = true;
+        }
         document.body.style.height = '100%';
         document.body.style.overflow = 'hidden';
-
         draggableBtnRef.current.style.opacity = 1;
     }, []);
 
-    const handleTouchMove = useCallback(
-        (event) => {
-            event.stopPropagation();
+    const handleMove = useCallback((event) => {
+        event.stopPropagation();
+        if(!isTouch && !isMouseDownRef.current) return;
+        const clientX = isTouch ? event.changedTouches[0].clientX : event.clientX;
+        const clientY = isTouch ? event.changedTouches[0].clientY : event.clientY;
 
-            const { clientX, clientY } = event.changedTouches[0];
+        const { left, right, top, bottom } = closeBtnRef.current.getBoundingClientRect();
 
-            const closePositionObj =
-                closeBtnRef.current.getBoundingClientRect();
-            const { left, right, top, bottom } = closePositionObj;
+        const { clientWidth, clientHeight, style } = draggableBtnRef.current;
+        const topPosition = clientY - clientHeight / 2;
+        const leftPosition = clientX - clientWidth / 2;
+        const bottomPosition = clientY + clientHeight / 2;
+        const rightPosition = clientX + clientWidth / 2;
 
-            const { clientWidth, clientHeight, style } =
-                draggableBtnRef.current;
-            const topPosition = clientY - clientHeight / 2;
-            const leftPosition = clientX - clientWidth / 2;
-            const bottomPosition = clientY + clientHeight / 2;
-            const rightPosition = clientX + clientWidth / 2;
+        const closeButtonHovered =
+            bottomPosition > top &&
+            topPosition < bottom &&
+            rightPosition > left &&
+            leftPosition < right;
 
-            const closeButtonHovered =
-                bottomPosition > top &&
-                topPosition < bottom &&
-                rightPosition > left &&
-                leftPosition < right;
+        if (closeButtonHovered) {
+            closeBtnRef.current.style.transform = 'translate(-50%, 0) scale(1.2)';
+            isCloseButtonHoveredRef.current = true;
+        } else {
+            closeBtnRef.current.style.transform = 'translate(-50%, 0)';
+            isCloseButtonHoveredRef.current = false;
+        }
 
-            if (closeButtonHovered) {
-                closeBtnRef.current.style.transform =
-                    'translate(-50%, 0) scale(1.2)';
-                isCloseButtonHoveredRef.current = true;
+        style.top = `${topPosition}px`;
+        style.left = `${leftPosition}px`;
+        style.right = `${window.innerWidth - rightPosition}px`;
+
+        if (!isDragging) {
+            style.transition = null;
+            setIsDragging(true);
+        }
+    }, [isDragging, isTouch]);
+
+    const handleEnd = useCallback((event) => {
+        if(!isTouch) {
+            isMouseDownRef.current = false;
+        }
+        const clientX = isTouch ? event.changedTouches[0].clientX : event.clientX;
+        const clientY = isTouch ? event.changedTouches[0].clientY : event.clientY;
+        const { clientHeight, style } = draggableBtnRef.current;
+        const windowHeight = window.innerHeight;
+
+        if (isDragging) {
+            const shouldStickonLeft = clientX < window.innerWidth / 2;
+
+            let yPosition;
+            if (windowHeight - clientY < threshold) {
+                yPosition = windowHeight - threshold;
+            } else if (windowHeight - clientY > windowHeight - threshold) {
+                yPosition = threshold;
             } else {
-                closeBtnRef.current.style.transform = 'translate(-50%, 0)';
-                isCloseButtonHoveredRef.current = false;
+                yPosition = clientY;
             }
 
-            style.top = `${topPosition}px`;
-            style.left = `${leftPosition}px`;
-            style.right = `${window.innerWidth - rightPosition}px`;
+            style.top = `${yPosition - clientHeight / 2}px`;
+            style.left = shouldStickonLeft ? xPositionValue : null;
+            style.right = !shouldStickonLeft ? xPositionValue : null;
+            draggableBtnRef.current.style.opacity = 0.5;
+            style.transition = `inset 0.5s, opacity 0.2s linear ${blurredBtnDelay / 1000}s`;
 
-            if (!isDragging) {
-                style.transition = null;
-                setIsDragging(true);
+            if (isCloseButtonHoveredRef.current && onClose) {
+                onClose();
             }
-        },
-        [isDragging]
-    );
+            setIsDragging(false);
+        } else if (onClick) {
+            onClick(event);
+        }
 
-    const handleTouchEnd = useCallback(
-        (event) => {
-            const { clientX, clientY } = event.changedTouches[0];
-            const { clientHeight, style } = draggableBtnRef.current;
-            const windowHeight = window.innerHeight;
-
-            if (isDragging) {
-                const shouldStickonLeft = clientX < window.innerWidth / 2;
-
-                let yPosition;
-                if (windowHeight - clientY < threshold) {
-                    yPosition = windowHeight - threshold;
-                } else if (windowHeight - clientY > windowHeight - threshold) {
-                    yPosition = threshold;
-                } else {
-                    yPosition = clientY;
-                }
-
-                style.top = `${yPosition - clientHeight / 2}px`;
-                style.left = shouldStickonLeft ? xPositionValue : null;
-                style.right = !shouldStickonLeft ? xPositionValue : null;
-                draggableBtnRef.current.style.opacity = 0.5;
-                style.transition = `inset 0.5s, opacity 0.2s linear ${blurredBtnDelay / 1000}s`;
-
-                if (isCloseButtonHoveredRef.current && onClose) {
-                    onClose();
-                }
-                setIsDragging(false);
-            } else if (onClick) {
-                onClick(event);
-            }
-
-            document.body.style.height = 'auto';
-            document.body.style.overflow = 'auto';
-        },
-        [threshold, blurredBtnDelay, onClose, onClick]
-    );
+        document.body.style.height = 'auto';
+        document.body.style.overflow = 'auto';
+    }, [isDragging, isTouch, threshold, blurredBtnDelay, onClose, onClick]);
 
     const initialPositionStyles = {
         top: yPositionValue,
         transition: `opacity 0.2s linear ${blurredBtnDelay / 1000}s`,
-        ...(align === 'right'
-            ? { right: xPositionValue }
-            : { left: xPositionValue }),
+        ...(align === 'right' ? { right: xPositionValue } : { left: xPositionValue }),
     };
 
     return (
@@ -163,9 +163,11 @@ function DraggableButton({
             <div
                 style={initialPositionStyles}
                 className={`container ${isVisible ? '' : 'display-none'} ${className}`}
-                onTouchStart={handleTouchStart}
-                onTouchMove={handleTouchMove}
-                onTouchEnd={handleTouchEnd}
+                onTouchStart={handleStart}
+                onTouchMove={handleMove}
+                onTouchEnd={handleEnd}
+                onMouseDown={handleStart}
+                onMouseUp={handleEnd}
                 ref={draggableBtnRef}
             >
                 {children}
